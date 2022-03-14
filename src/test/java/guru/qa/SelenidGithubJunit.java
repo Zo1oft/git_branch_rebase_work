c24698c73e1323ec12bb25411b63540327d1bbb3
Max Zhurkin <tryzoloft@gmail.com>
2022-03-14 16:26:45 +0500

    removed commentaries

--- src/test/java/guru/qa/SelenidGithubJunit.java
+++ src/test/java/guru/qa/SelenidGithubJunit.java
@@ -17,24 +17,13 @@ public class SelenidGithubJunit {
         Configuration.browserSize = "1920x1080";
     }
 
-    // Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
-    // Есть, в первом варианте будут выбираться h1 во всеъ div, во втором случае ищется div только в первом h1.
-
     @Test
     void shouldFindJunit5() {
-        // открыть страницу github.com
         open("https://github.com/selenide/selenide");
-        // нажимаем на линк на Wiki
         $("#wiki-tab").click();
-
-        // раскрываем список Pages
         $("#wiki-pages-box").$(withText("more pages")).click();
-        // проверяем, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-        // переходим на страницу SoftAssertions
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        // проверяем, что на странице есть пример кода для JUnit5
         $("#wiki-body").shouldHave(text("JUnit5"));
-        //$$("#wiki-body").shouldHave(textsInAnyOrder("JUnit5", "Example", "code")); - не работает :)
     }
 }
